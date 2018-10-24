92b8396489b666eecac69bd55bc011f0809384e0
Caleb Trachte <cetrachte@hotmail.com>
2018-10-24 16:00:33 -0500

    add delete contact method and specs

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -2,8 +2,15 @@ function AddressBook () {
   this.contacts = [];
   this.addContact = (contact) => {
     this.contacts.push(contact);
-  }
+  };
   this.getContact = (contact) => {
     return this.contacts[contact];
-  }
+  };
+  this.deleteContact = (index) => {
+    if (this.contacts){
+      this.contacts.splice(index,1);
+    }
+    else {return;}
+  };
+
 };
